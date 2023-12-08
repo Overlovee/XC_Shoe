766314b16f181ce6a32ccb0333a74cbdd52d7006
I'mFoodboy <vnn159357@gmail.com>
2023-12-08 11:10:14 +0700

    update

--- XC_Shoe/Controllers/LoginController.cs
+++ XC_Shoe/Controllers/LoginController.cs
@@ -149,7 +149,7 @@ namespace XC_Shoe.Controllers
             else
             {
                 ViewBag.ThongBao = "Invalid email or password.";
-                return RedirectToAction("Sigin", "Login");
+                return RedirectToAction("SingIn", "Login");
             }
         }
         private bool IsValidLogin(string email, string password)
@@ -191,7 +191,7 @@ namespace XC_Shoe.Controllers
             {
                 return RedirectToAction("ShowStartAdminPage", "Admin");
             }
-            return RedirectToAction("ShowHomePage", "Login");
+            return RedirectToAction("ShowHomePage", "User");
         }
     }
 }
